683bcf17818dee5e230c835a7f52eaebbbad642d
ashishkantk15 <ashishkant181@gmail.com>
2024-09-02 16:18:21 +0530

    Initial Commit

---
+++ welcome/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  welcome
+//
+//  Created by Ashishkant on 02/09/24.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    string str;
+    cout<<"may i know your name";
+    getline(cin,str);
+    cout<<"hello"<<str;
+    return 0;
+}
